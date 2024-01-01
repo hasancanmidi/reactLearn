ae54dc7f7cb2fc70ee875e4c774e1bdf06562919
mac <Hasancanmidi@hotmail.com>
2024-01-01 22:37:25 +0300

    Update

--- berkant-kaya-react/src/App.jsx
+++ berkant-kaya-react/src/App.jsx
@@ -1,44 +1,19 @@
-import Text from './components/Text'
-import Button from './components/Button'
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
 
 function App() {
 
-  const [name, setName] = useState(null)
-  const [data, setData] = useState([])
-
-  //const [değişken, değişkeni setlemek istediğin fonksiyon] = useState("DEFAULF")
-  console.log(name, "name")
-
-  const clickFunc = () =>{
-    setData(p => [...p,name])
-  }
-
-  console.log(data, "data")
-
   return (
-    <div>
-      <input type="text" onChange={(d) => setName(d.target.value)}/>
-      <Button buttonName={"Tıkla"} onClick={clickFunc}/>
-      <div>
-        {
-          data.map((gez,index) => (
-            <div key={index}>{gez}</div>
-          ))
-        }
-      </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/detail' element={<Detail/>}/>
+        </Routes>
+      </Router>
 
-      {/* <Button buttonName={"Azalt"} onClick={() => setCount(count - 1)} />
-      <div>{count}</div>
-      <Button buttonName={"Artır"} onClick={() => setCount(count + 1)} /> */}
-
-       {/* <Text number="1" lesson={"React Eğitimi"}/>
-       <Text number="2" lesson={"C# Eğitimi"}/>
-       <Text number="3" lesson={"Java Eğitimi"}/>
-       <Button buttonName={"Ekle"}/>
-       <Button buttonName={"Sil"}/>
-       <Button buttonName={"Güncelle"}/> */}
-    </div>
+    </>
   )
 }
 
