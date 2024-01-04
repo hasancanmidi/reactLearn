1250110c216cb794138029127256c9ba52bc90ea
mac <Hasancanmidi@hotmail.com>
2024-01-04 06:26:47 +0300

    update

--- learn-typescript/src/App.tsx
+++ learn-typescript/src/App.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import './App.css';
-
+import input from './components/Input'
 function App() {
-  let name : string = "hasan can";
+const[todo,setTodo] = useState<string>('')
+
+
+
+  // let name : string = "Hasan Can";
+  // let age : number = 25
+  // let done : boolean = true
+  // let trial : number | string = 5
+
+  // interface obj{
+  //   name: String,
+  //   age: number,
+  //   done?: boolean,
+  // }
+
+  // interface objTwo extends obj{
+  //   accept?: Boolean;
+  // }
+
+  // let obj : objTwo = {
+  //   name: "Hasan Can",
+  //   age: 25,
+  //   //done: true
+  // }
   return (
     <div className="App">
-      {name}
+      <input/>
     </div>
   );
 }
---
+++ learn-typescript/src/components/Input.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Yazınız..." />
+      <button>Ekle</button>
+    </div>
+  );
+};
+
+export default Input;
